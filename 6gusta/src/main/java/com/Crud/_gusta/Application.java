7f12b1c1a7111e4a774d4f640e41219fab22323a
Luiz Gustavo <luiz,carvalho7110@sounidesc,com,br>
2025-06-04 13:38:33 -0300

    admin

--- 6gusta/src/main/java/com/Crud/_gusta/Application.java
+++ 6gusta/src/main/java/com/Crud/_gusta/Application.java
@@ -1,7 +1,12 @@
 package com.Crud._gusta;
 
+import com.Crud._gusta.domain.model.CadastroOng;
+import com.Crud._gusta.infrastructure.repository.OngCadastroRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +15,22 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	CommandLineRunner createAdmin(OngCadastroRepository repository, PasswordEncoder encoder) {
+		return args -> {
+			String email = "admin@email.com";
+			if (!repository.existsByEmail(email)) {
+				CadastroOng admin = new CadastroOng();
+				admin.setNome("Admin");
+				admin.setEmail(email);
+				admin.setSenha(encoder.encode("admin123")); // senha criptografada
+				admin.setRole("ADMIN");
+
+				repository.save(admin);
+				System.out.println("Administrador padrão criado com sucesso!");
+			} else {
+				System.out.println("Administrador padrão já existe.");
+			}
+		};
+	}
 }
--- 6gusta/src/main/java/com/Crud/_gusta/infrastructure/repository/OngCadastroRepository.java
+++ 6gusta/src/main/java/com/Crud/_gusta/infrastructure/repository/OngCadastroRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface OngCadastroRepository  extends JpaRepository<CadastroOng, Long> {
 
      CadastroOng findByNome(String nome);
+     boolean existsByEmail(String email);
+
 }
